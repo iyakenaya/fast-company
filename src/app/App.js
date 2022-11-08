86cc4604d961637523555947186c789aefdf07c3
Natalia Nesterenko <2812natali@gmail.com>
2022-11-08 15:31:55 +0300

    added the ability to receive professions as an array, typing, useEffect Hook

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Users from "./components/users";
 import api from "./api";
 
 const App = () => {
-  const [users, setUsers] = useState(api.users.fetchAll());
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    api.users.fetchAll().then((data) => setUsers(data));
+  }, []);
 
   const handleDelete = (userId) => {
     setUsers(users.filter((user) => userId !== user._id));
@@ -20,7 +24,6 @@ const App = () => {
     );
     console.log(id);
   };
-
   return (
     <div>
       <Users
--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -8,7 +8,6 @@ const GroupList = ({
   onItemSelect,
   selectedItem
 }) => {
-  console.log(Object.keys(items));
   return (
     <ul className="list-group">
       {Object.keys(items).map((item) => (
@@ -33,7 +32,7 @@ GroupList.defaultProps = {
 };
 
 GroupList.propTypes = {
-  items: PropTypes.object.isRequired,
+  items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   valueProperty: PropTypes.string.isRequired,
   contentProperty: PropTypes.string.isRequired,
   onItemSelect: PropTypes.func,
