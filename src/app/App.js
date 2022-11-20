72daf847d104e8d835fc0dca78d74247911a71fa
Natalia Nesterenko <2812natali@gmail.com>
2022-11-20 16:33:03 +0300

    created a form, processed its submission, added validation, displayed a validation error, created a validator

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Users from "./layouts/users";
+import Login from "./layouts/login";
+import MainPage from "./layouts/main";
 import NavBar from "../app/components/navBar";
-import UserPage from "./components/userPage";
-import Users from "./components/users";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Switch>
-        <Route path="/" exact render={() => <h1>Main Page</h1>} />
-        <Route path="/login" render={() => <h1>Login</h1>} />
-        <Route path="/users/:userId" component={UserPage} />
-        <Route path="/users" component={Users} />
+        <Route path="/users/:userId?" component={Users} />
+        <Route path="/login" component={Login} />
+        <Route path="/" exact component={MainPage} />
+        <Redirect to="/" />
       </Switch>
     </div>
   );
